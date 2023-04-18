4b882b9325956b077a59a629d68e5ce2c14b795e
Noah Gonzalez <gnzalezmnoah@gmail.com>
2023-04-17 23:15:15 -0700

    update opacity

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
 	
 	var myCues = [
 		
-		{seconds: 17, callback: func1}, 
+		{seconds: 13, callback: func1}, 
 		// {seconds: 40, callback: func2}, 
 		
 		];
@@ -53,7 +53,6 @@ function togglePlay(){
 //function to update play and pause button 
 function updateButton(e){
     const togglePlayBtn = document.querySelector('#play_button');
-    console.log(video);
     if(video.paused){
         togglePlayBtn.innerHTML = 
         '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/> </svg>';
@@ -109,8 +108,6 @@ function rangeUpdate(e){
     video.volume = e.currentTarget.value /100;
     audioOrig.volume = e.currentTarget.value /100;
     backgroundMusic.volume = e.currentTarget.value /100;
-    console.log(video.volume);
-    console.log(e.currentTarget);
     return(e.currentTarget);
 }
 function scrub(e){
@@ -150,18 +147,15 @@ function backVolume(){
         backgroundMusic.volume = 1;
         //backgroundMusic.volume = rangeUpdate();
         backButn.innerHTML='Background  is ON';
-        console.log(backgroundMusic.volume);
-
     }
     else if (backgroundMusic.muted ===false){
         backgroundMusic.muted = true;
         backgroundMusic.volume = 0;
         backButn.innerHTML='Background Music is OFF';
-        console.log(backgroundMusic.volume);
     }
 }
 function func1(){
-    addClass('.chill-coffee', '.opacity-change')
+    chill.setAttribute('id','opacity-change');
 }
 
 // //Toggle Original to Foley
